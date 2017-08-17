d19dc7c8eeefbb37d8fc7af5875f390417a41b08
Andrew Lohr <alohr51@users.noreply.github.com>
2017-08-16 21:08:37 -0400

    init

--- app.js
+++ app.js
@@ -138,9 +138,9 @@ app.get('/hotdog', upload.single('maybe-hotdog-image'), function(req, res) {
 function formatBytes(bytes, decimals) {
 	if(bytes == 0) return '0 Bytes';
 	var k = 1000;
-	var	dm = decimals || 2;
-	var	sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	var	i = Math.floor(Math.log(bytes) / Math.log(k));
+	var dm = decimals || 2;
+	var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	var i = Math.floor(Math.log(bytes) / Math.log(k));
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
